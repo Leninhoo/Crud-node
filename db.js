1efe10bce1a62e5032d0e79155f6f3194cf92f10
Leninhoo <andre_cruz13@live.com>
2026-03-19 12:24:05 -0300

    adicionando dotenv para compatibilidade com render

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
+import 'dotenv/config';
 import postgres from 'postgres'
 
-//const http = require("http");
-//const { neon } = require("@neondatabase/serverless");
-//const sql = neon(process.env.DATABASE_URL);
 
 export const sql = postgres(process.env.DATABASE_URL, { ssl: 'require' });
